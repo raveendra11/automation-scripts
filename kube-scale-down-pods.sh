c3b0c877c47d2c7e660022e244140414f8e1f6bf
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2026-04-09 10:46:52 -0500

    Fix variable name for scaling deployment

--- kube-scale-down-pods.sh
+++ kube-scale-down-pods.sh
@@ -4,9 +4,9 @@ NAMESPACE=default
 DEPLOYMENT_NAME=dogood
 
 echo "Scaling deployment to 0 replica..."
-kubectl scale deployment $dogood --replicas=0 -n $NAMESPACE
+kubectl scale deployment $DEPLOYMENT_NAME --replicas=0 -n $NAMESPACE
 
 echo "Waiting for rollout to finish..."
-kubectl rollout status deployment $dogood -n $NAMESPACE
+kubectl rollout status deployment $DEPLOYMENT_NAME -n $NAMESPACE
 
 echo "Done."
