e47dbee7dd32c9e30077e54491781d2464f9aae2
Raveendra Pujari <72726513+raveendra11@users.noreply.github.com>
2026-04-09 10:45:43 -0500

    Update kube-scale-pods.sh

--- kube-scale-pods.sh
+++ kube-scale-pods.sh
@@ -4,9 +4,9 @@ NAMESPACE=default
 DEPLOYMENT_NAME=dogood
 
 echo "Scaling deployment to 1 replica..."
-kubectl scale deployment $dogood --replicas=1 -n $NAMESPACE
+kubectl scale deployment $DEPLOYMENT_NAME --replicas=1 -n $NAMESPACE
 
 echo "Waiting for rollout to finish..."
-kubectl rollout status deployment $dogood -n $NAMESPACE
+kubectl rollout status deployment $DEPLOYMENT_NAME -n $NAMESPACE
 
 echo "Done."
